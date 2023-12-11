4d90eb0c799c55e76267ecc2afd4c066708769d2
AlexRibeiro <alexmtt.ribeiro@gmail.com>
2023-12-11 14:00:50 -0300

    Incluido função de tela de autenticação

--- agenda/urls.py
+++ agenda/urls.py
@@ -19,8 +19,12 @@ from django.urls import path
 from core import views
 from django.views.generic import RedirectView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    path('', RedirectView.as_view(url='/agenda/'))
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
